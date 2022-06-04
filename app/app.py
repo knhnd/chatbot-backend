3d946cb167c9bcd80186c7e94b45b441dcafaa6c
Ken Honda <ken.h.0802@gmail.com>
2022-06-04 18:22:25 +0900

    [UPDATE] update app.py

--- app/app.py
+++ app/app.py
@@ -46,5 +46,5 @@ def test():
 #         event.reply_token,
 #         TextSendMessage(text=event.message.text))
 
-# if __name__ == "__main__":
-#     app.run()
+if __name__ == "__main__":
+    app.run()
