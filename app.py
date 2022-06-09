748d7340c2a1f2a6bae95d7834cc9685cc7c6591
Ken Honda <ken.h.0802@gmail.com>
2022-06-09 20:17:10 +0900

    [UPDATE] update app file

--- app.py
+++ app.py
@@ -31,6 +31,7 @@ def test():
 # 認証情報の検証
 @app.route("/callback", methods=['POST'])
 def callback():
+    print("in callback")
     # get X-Line-Signature header value
     signature = request.headers['X-Line-Signature']
 
@@ -44,11 +45,11 @@ def callback():
     except InvalidSignatureError:
         print("Invalid signature. Please check your channel access token/channel secret.")
         abort(400)
-
     return 'OK'
 
 @handler.add(MessageEvent, message=TextMessage)
 def handle_message(event):
+    print("in handle")
     line_bot_api.reply_message(
         event.reply_token,
         TextSendMessage(text=event.message.text))
