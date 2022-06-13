84f72bd0ca6395d83c679661cbdd689ad0ec10ed
Ken Honda <ken.h.0802@gmail.com>
2022-06-13 19:42:06 +0900

    [UPDATE] update service class

--- src/services/handle_message_service.py
+++ src/services/handle_message_service.py
@@ -2,6 +2,6 @@ import json
 
 class handle_message_service : 
     def generate_reply_message(receivedMessage) :
-        json_file = open("../dict/dict.json", "r")
-        print(json_file)
-        return receivedMessage + "OK"
+        json_file = open("./../dict/dict.json", "r")
+        json_data = json.load(json_file)
+        return json_data
