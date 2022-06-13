cb82008cb60a85fba8357c2fee12461357c75f12
Ken Honda <ken.h.0802@gmail.com>
2022-06-13 20:02:53 +0900

    [UPDATE] update service class

--- src/services/handle_message_service.py
+++ src/services/handle_message_service.py
@@ -10,4 +10,12 @@ word = {
 
 class handle_message_service : 
     def generate_reply_message(receivedMessage) :
-        return word['hello']
+        if (receivedMessage == 'こんにちは'):
+            return word['afternoon']
+        elif (receivedMessage == 'hello'):
+            return word['hello']
+        elif (receivedMessage == 'こんにちは') :
+            return word['test']
+        else:
+            return '辞書にない単語です'
+
