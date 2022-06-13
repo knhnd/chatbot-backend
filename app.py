b36ec05c801ba966ecbeff6a0276c9916e4c0924
Ken Honda <ken.h.0802@gmail.com>
2022-06-13 12:10:41 +0900

    [UPDATE] update app.py

--- app.py
+++ app.py
@@ -34,11 +34,9 @@ def callback():
     print()
     # get X-Line-Signature header value
     signature = request.headers['X-Line-Signature']
-
     # get request body as text
     body = request.get_data(as_text=True)
     app.logger.info("Request body: " + body)
-
     # handle webhook body
     try:
         handler.handle(body, signature)
@@ -47,12 +45,12 @@ def callback():
         abort(400)
     return 'OK'
 
+# handle message from LINE
 @handler.add(MessageEvent, message=TextMessage)
 def handle_message(event):
-    print("in handle")
     line_bot_api.reply_message(
         event.reply_token,
-        TextSendMessage(text=event.message.text))
+        TextSendMessage(text=event))
 
 if __name__ == "__main__":
     app.run()
