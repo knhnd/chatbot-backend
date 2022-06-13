b776e37f7725f3de4a7698a1f5bf8f22725390c2
Ken Honda <ken.h.0802@gmail.com>
2022-06-13 12:12:50 +0900

    [UPDATE] update app.py

--- app.py
+++ app.py
@@ -48,9 +48,10 @@ def callback():
 # handle message from LINE
 @handler.add(MessageEvent, message=TextMessage)
 def handle_message(event):
+    print(event)
     line_bot_api.reply_message(
         event.reply_token,
-        TextSendMessage(text=event))
+        TextSendMessage(text=event.message.text))
 
 if __name__ == "__main__":
     app.run()
