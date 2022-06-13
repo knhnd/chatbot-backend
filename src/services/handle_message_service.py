4d064a39d08fffc0f3a996c3ec2ffb3a6f439218
Ken Honda <ken.h.0802@gmail.com>
2022-06-13 19:53:03 +0900

    [UPDATE] update service class

--- src/services/handle_message_service.py
+++ src/services/handle_message_service.py
@@ -2,12 +2,12 @@ dict = {
   "test": "正常に可動しています！",
   "sample": "ちゃんと受け取れています！",
   "hello": "Hello, World!",
-  "おはよう": "おはようございます，よい朝ですね！",
-  "こんにちは": "こんにちは，ごきげんいかがですか？",
-  "こんばんは": "こんばんは，いい夜をお過ごしください！"
+  "morning": "おはようございます，よい朝ですね！",
+  "afternoon": "こんにちは，ごきげんいかがですか？",
+  "evening": "こんばんは，いい夜をお過ごしください！"
 }
 
 
 class handle_message_service : 
     def generate_reply_message(receivedMessage) :
-        return dict
+        return dict.hello
