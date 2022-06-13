51c9a9de4b0aa6365329c76eb79ddffab1bc1b0d
Ken Honda <ken.h.0802@gmail.com>
2022-06-13 19:49:46 +0900

    [UPDATE] update service class

--- src/services/handle_message_service.py
+++ src/services/handle_message_service.py
@@ -1,7 +1,13 @@
-import json
+dict = {
+  "test": "正常に可動しています！",
+  "sample": "ちゃんと受け取れています！",
+  "hello": "Hello, World!",
+  "おはよう": "おはようございます，よい朝ですね！",
+  "こんにちは": "こんにちは，ごきげんいかがですか？",
+  "こんばんは": "こんばんは，いい夜をお過ごしください！"
+}
+
 
 class handle_message_service : 
     def generate_reply_message(receivedMessage) :
-        json_file = open("./../dict/dict.json", "r")
-        json_data = json.load(json_file)
-        return json_data
+        return dict
