ae6a1a3cdd5388dce9e945a40d9a8927f7974abc
Ken Honda <ken.h.0802@gmail.com>
2022-06-14 15:41:35 +0900

    [UPDATE] update handle_message_service

--- src/services/handle_message_service.py
+++ src/services/handle_message_service.py
@@ -1,15 +1,10 @@
-word = {
-  "test": "正常に可動しています！",
-  "sample": "ちゃんと受け取れています！",
-  "hello": "Hello, World!",
-  "morning": "おはようございます，よい朝ですね！",
-  "afternoon": "こんにちは，ごきげんいかがですか？",
-  "evening": "こんばんは，いい夜をお過ごしください！"
-}
-
+import pickle
 
 class handle_message_service : 
     def generate_reply_message(receivedMessage) :
+        with open('./../dict/mydict.pkl', mode='rb') as f:
+            mydict = pickle.load(f)
+        print(mydict)
         if (receivedMessage == 'こんにちは'):
             return word['afternoon']
         elif (receivedMessage == 'hello'):
