4b9705c15706a425fa8536e491d430087594fc8e
Ken Honda <ken.h.0802@gmail.com>
2022-06-14 16:39:03 +0900

    [UPDATE] update handle_message_service

--- src/services/handle_message_service.py
+++ src/services/handle_message_service.py
@@ -4,6 +4,9 @@ class handle_message_service :
     def generate_reply_message(receivedMessage) :
         json_file = open('/app/src/dict/mydict.json', 'r')
         mydict = json.load(json_file)
-        print(mydict)
-        return receivedMessage
 
+        for key in mydict :
+            if (receivedMessage == key) :
+                return mydict[key]
+            else :
+                return mydict['except']
