6990f353dc646b945cba081a81055b91041de96f
Ken Honda <ken.h.0802@gmail.com>
2022-06-14 16:07:36 +0900

    [UPDATE] update handle_message_service

--- src/services/handle_message_service.py
+++ src/services/handle_message_service.py
@@ -2,7 +2,7 @@ import pickle
 
 class handle_message_service : 
     def generate_reply_message(receivedMessage) :
-        with open('../dict/mydict.pkl', mode='rb') as f:
+        with open('mydict.pkl', mode='rb') as f:
             mydict = pickle.load(f)
         print(mydict)
         return receivedMessage
