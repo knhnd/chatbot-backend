b4d02e620781d4db0b636277903894839e8976c2
Ken Honda <ken.h.0802@gmail.com>
2022-06-14 16:29:07 +0900

    [UPDATE] update handle_message_service

--- src/services/handle_message_service.py
+++ src/services/handle_message_service.py
@@ -2,7 +2,7 @@ import json
 
 class handle_message_service : 
     def generate_reply_message(receivedMessage) :
-        json_file = open('/src/dict/mydict.json', 'r')
+        json_file = open('/app/src/dict/mydict.json', 'r')
         mydict = json.load(json_file)
         print(mydict)
         return receivedMessage
